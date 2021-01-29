c4bb2ae1e73f7e5f0066c3c15a13ba20cc03de51
Marlies Vervloet <marlies.vervloet@ppw.kuleuven.be>
2021-01-29 09:10:08 +0000

    version 2.7.1

--- R/SortLoadings.R
+++ R/SortLoadings.R
@@ -1,23 +1,27 @@
 SortLoadings <-
   function(Px){
-    R <- nrow(Px)
+    Px_t = as.data.frame(t(Px))
+    
+    R <- ncol(Px_t)
     index <- 1
     
-    maxi <- apply(abs(Px),2,which.max)
-    sorted <- rbind(Px,maxi)
-    order <- sort(abs(sorted[R+1,]),decreasing=F)
-    IX <- colnames(order)
-    sorted <- sorted[,IX]
+    maxi <- apply(abs(Px_t),1,which.max)
+    sorted <- cbind(Px_t,maxi)
+    order <- sorted[ order(abs(sorted[,R+1]),decreasing=F), ]
+    IX <- rownames(order)
+    sorted <- sorted[IX,]
     name <- NULL
     
-    for (r in 1:nrow(Px)){
-      part <- sort(abs(sorted[r,sorted[R+1,]==r]),decreasing=T)
-      IX <- colnames(part)
-      block <- sorted[,IX]
-      Px[,index:(index+length(IX)-1)] <- block[1:R,]
-      name <- c(name,colnames(block))
+    for (r in 1:ncol(Px_t)){
+      part <- sorted[ order(abs(sorted[sorted[,R+1]==r, r]),decreasing=T) + index - 1, ]
+      IX <- rownames(part)
+      block <- sorted[IX,1:R]
+      Px_t[index:(index+length(IX)-1),] <- block
+      name <- c(name,IX)
       index <- index+length(IX)
     }
-    colnames(Px) <- name
+    rownames(Px_t) <- name
+    Px = as.data.frame(t(Px_t))
     return(Px)
   }
+
